0e8fa3d41340b82f9f2b12e358f6f15e19b8e75e
Paul Geppert <paul.geppert@student.hpi.de>
2021-05-14 12:48:20 +0200

    Deactivated wifi 6 due to incompatible ns-3 version

--- marvis/channel/wifi.py
+++ marvis/channel/wifi.py
@@ -61,7 +61,8 @@ class WiFiChannel(Channel):
         #: Standard from 2013.
         WIFI_802_11ac = wifi.WIFI_PHY_STANDARD_80211ac
         #: "WiFi 6".
-        WIFI_802_11ax = wifi.WIFI_PHY_STANDARD_80211ax
+        # Deactivated due to incompatible ns-3 version
+        # WIFI_802_11ax = wifi.WIFI_PHY_STANDARD_80211ax
         #: Wireless Access in Vehicular Environments (WAVE).
         WIFI_802_11p = wifi.WIFI_PHY_STANDARD_80211_10MHZ
 
